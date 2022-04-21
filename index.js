174ec09d5b5b6996e53be8e6acc4c75a55270629
S60229 <103551596+S60229@users.noreply.github.com>
2022-04-21 22:37:49 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(function()
+{
+    button = $("#button_change");
+
+    $(button).click(function()
+    {
+        var input = $("#input_color");
+
+        var inputValue = $(input).val();
+
+        var inputValue = $("#input_color").val();
+
+        var element = $(".cow_color");
+
+        $(element).html(inputValue);
+    });
+});
